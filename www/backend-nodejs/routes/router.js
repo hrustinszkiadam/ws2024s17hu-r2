7ab6236fc887cc068fdc81a68209f8db53587040
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-04 18:53:19 +0200

    feat: /login

--- www/backend-nodejs/routes/router.js
+++ www/backend-nodejs/routes/router.js
@@ -1,5 +1,33 @@
 import { Router } from 'express';
+import db from '../db.js';
 
 const router = Router();
+
+router.post(
+	'/login',
+	(req, res, next) => {
+		const { token } = req.body;
+		if (!token) return next();
+
+		db.query(
+			'SELECT * FROM runners WHERE token = ?',
+			[token],
+			(err, results) => {
+				if (err || !results.length) return next();
+
+				return res.send({
+					status: 'success',
+					user: results[0],
+				});
+			}
+		);
+	},
+	(_, res) => {
+		res.status(401).send({
+			status: 'error',
+			message: 'Login failed',
+		});
+	}
+);
 
 export default router;
