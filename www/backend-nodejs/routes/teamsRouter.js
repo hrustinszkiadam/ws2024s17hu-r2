088f3d69dd7da004c1d99c5951a9bfb9f89d884b
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-04 19:23:29 +0200

    chore: removed 404 line

--- www/backend-nodejs/routes/teamsRouter.js
+++ www/backend-nodejs/routes/teamsRouter.js
@@ -4,11 +4,9 @@ import db from '../db.js';
 const teamsRouter = Router();
 
 teamsRouter.get('/', (req, res) => {
-	db.query('SELECT * FROM teams WHERE id = ?', req.teamId, (_, results) => {
-		if (!results.length) return res.sendStatus(404);
-
-		res.send(results[0]);
-	});
+	db.query('SELECT * FROM teams WHERE id = ?', req.teamId, (_, results) =>
+		res.send(results[0])
+	);
 });
 
 export default teamsRouter;
