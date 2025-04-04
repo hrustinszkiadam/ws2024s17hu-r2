e93ec73ccb82204fa9f0b26a57b12834fb3598ce
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-04-04 22:25:55 +0200

    fix: Adjust layout and spacing for improved UI consistency on Home page

--- www/team-admin.src/src/pages/Home.tsx
+++ www/team-admin.src/src/pages/Home.tsx
@@ -4,12 +4,23 @@ import ManageTeam from '../components/ManageTeam';
 const Home = () => {
 	return (
 		<div className='w-screen h-screen p-10'>
-			<div className='flex justify-between grow items-center pr-10'>
-				<h1 className='text-5xl font-semibold mb-5'>Manage your team</h1>
+			<div className='flex justify-between grow items-center pr-10 mb-5'>
+				<h1 className='text-5xl font-semibold'>Manage your team</h1>
 				<LogoutButton />
 			</div>
 			<ManageTeam />
 			<hr className='my-5 border' />
+			<div className='flex justify-between grow items-center pr-10 mb-5'>
+				<h2 className='text-5xl font-semibold'>Runners</h2>
+				<button
+					className='btn'
+					onClick={() =>
+						window.open('http://stage-planner.localhost', '_blank')
+					}
+				>
+					Stage Planner
+				</button>
+			</div>
 		</div>
 	);
 };
